ae2120f831847e87edd574349cbf4569df65f264
yasin <ysndlklc1989@icloud.com>
2023-10-24 00:27:20 +0300

    Initial Commit

---
+++ CarExpertiesLetGo/CarExpertiesLetGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarExpertiesLetGoApp.swift
+//  CarExpertiesLetGo
+//
+//  Created by yasin on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CarExpertiesLetGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
